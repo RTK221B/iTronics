d69ac7f37878ad7e1c12468b57fe5a2cc876f1cd
ishwariy joshi <ishwariyjoshi1998@gmail.com>
2020-01-19 07:21:32 +0530

    DHT

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,63 @@
-#include <Arduino.h>
-#include <buzzer.h>
-#include <led.h>
-#include <accelerometer.h>
-#include <battreyStetus.h>
+//#include <Arduino.h>
+//#include <buzzer.h>
+//#include <led.h>
+//#include <accelerometer.h>
+//#include <battreyStetus.h>
 
+/*
+This example shows how to connect to Cayenne using an ESP32 and send/receive sample data.
 
-ADC_MODE(ADC_VCC);
+The CayenneMQTT Library is required to run this sketch. If you have not already done so you can install it from the Arduino IDE Library Manager.
+
+Steps:
+1. If you have not already installed the ESP32 Board Package install it using the instructions here: https://github.com/espressif/arduino-esp32/blob/master/README.md#installation-instructions.
+2. Select your ESP32 board from the Tools menu.
+3. Set the Cayenne authentication info to match the authentication info from the Dashboard.
+4. Set the network name and password.
+5. Compile and upload the sketch.
+6. A temporary widget will be automatically generated in the Cayenne Dashboard. To make the widget permanent click the plus sign on the widget.
+*/
+
+//#define CAYENNE_DEBUG
+#define CAYENNE_PRINT Serial
+#include <CayenneMQTTESP32.h>
+
+// WiFi network info.
+char ssid[] = "speechbee";
+char wifiPassword[] = "speechbee";
+
+// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
+char username[] = "4f9e03c0-3a28-11ea-b73d-1be39589c6b2";
+char password[] = "532fd65309de6b9d89deffb16ba08d774d4f25d0";
+char clientID[] = "5917b450-3a28-11ea-b73d-1be39589c6b2";
 
 void setup()
 {
   Serial.begin(115200);
+  Cayenne.begin(username, password, clientID, ssid, wifiPassword);
 }
-
-rom_phy_get_vdd33()
 
 void loop()
 {
-  int vcc = ESP.getVcc();
-  Serial.println(vcc);
-  delay(1000);
+  Cayenne.loop();
+}
+
+// Default function for sending sensor data at intervals to Cayenne.
+// You can also use functions for specific channels, e.g CAYENNE_OUT(1) for sending channel 1 data.
+CAYENNE_OUT_DEFAULT()
+{
+  // Write data to Cayenne here. This example just sends the current uptime in milliseconds on virtual channel 0.
+  Cayenne.virtualWrite(0, millis());
+  // Some examples of other functions you can use to send data.
+  //Cayenne.celsiusWrite(1, 22.0);
+  //Cayenne.luxWrite(2, 700);
+  //Cayenne.virtualWrite(3, 50, TYPE_PROXIMITY, UNIT_CENTIMETER);
+}
+
+// Default function for processing actuator commands from the Cayenne Dashboard.
+// You can also use functions for specific channels, e.g CAYENNE_IN(1) for channel 1 commands.
+CAYENNE_IN_DEFAULT()
+{
+  CAYENNE_LOG("Channel %u, value %s", request.channel, getValue.asString());
+  //Process message here. If there is an error set an error message using getValue.setError(), e.g getValue.setError("Error message");
 }
